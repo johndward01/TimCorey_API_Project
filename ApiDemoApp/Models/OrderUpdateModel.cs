4ae70891191b9009d98eca1377c37c3e45a81c01
johnd <johndward01@yahoo.com>
2020-10-01 15:41:35 -0500

    initial commit

---
+++ ApiDemoApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDemoApp.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+    }
+}
